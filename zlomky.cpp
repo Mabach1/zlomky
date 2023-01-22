70fd2dcf8ec7f0e31870ad9d69b727a5c2cf1d04
Mabach1 <105239325+Mabach1@users.noreply.github.com>
2023-01-22 19:54:13 +0100

    Update zlomky.cpp

--- zlomky.cpp
+++ zlomky.cpp
@@ -18,17 +18,17 @@
  * @return int   
  */
 int abs(int cislo) {
-	if (cislo < 0) { return cislo * -1; }
-	
-	return cislo;
+    if (cislo < 0) { return cislo * -1; }
+
+    return cislo;
 }
 /**
  * @brief Construct a new Zlomek:: Zlomek object
  * 
  */
 Zlomek::Zlomek(void) {
-	this->citatel = 0;
-	this->jmenovatel = 1;
+    this->citatel = 0;
+    this->jmenovatel = 1;
 }
 /**
  * @brief metoda, ktera najde spolecneho delitele
@@ -38,37 +38,37 @@ Zlomek::Zlomek(void) {
  * @return spolecny delitel 
  */
 int delitel(const int& cislo1,const int& cislo2) {
-	int max = (abs(cislo1) > abs(cislo2)) ? abs(cislo1) : abs(cislo2);  // nastaveni maximalni hodnoty, do ktere pujde smycka for
-	int found_delitel = 1;
-	
-	for (int i = 1; i < max; i++) {
-		if ((abs(cislo1) % i == 0)  && (abs(cislo2) % i == 0)) {
-			found_delitel = i;
-		}
-	}
-	
-	return found_delitel;
+    int max = (abs(cislo1) > abs(cislo2)) ? abs(cislo1) : abs(cislo2);  // nastaveni maximalni hodnoty, do ktere pujde smycka for
+    int found_delitel = 1;
+
+    for (int i = 1; i < max; i++) {
+        if ((abs(cislo1) % i == 0)  && (abs(cislo2) % i == 0)) {
+            found_delitel = i;
+        }
+    }
+
+    return found_delitel;
 }
 /**
  * @brief funkce upravi zlomek do zakladniho tvru 
  * 
  */
 void Zlomek::zkrat(void) {
-	int spolecny_delitel = delitel(this->citatel, this->jmenovatel);    // nalazeny spolecny delitel pomoci funkce delitel()
-	
-	this->citatel = this->citatel / spolecny_delitel;
-	this->jmenovatel = this->jmenovatel / spolecny_delitel;
-	
-	// ji-li citatel i jmenovatel zporny, celkovy zlomek je kladny a muzeme se tedy zbavit znamenek
-	if (this->citatel < 0 && this->jmenovatel < 0) { 
-		this->citatel    *= -1; 
-		this->jmenovatel *= -1; 
-	} 
-	
-	if (this->jmenovatel < 0 && this->citatel > 0) {
-		this->jmenovatel *= -1;
-		this->citatel    *= -1;
-	}
+    int spolecny_delitel = delitel(this->citatel, this->jmenovatel);    // nalazeny spolecny delitel pomoci funkce delitel()
+
+    this->citatel = this->citatel / spolecny_delitel;
+    this->jmenovatel = this->jmenovatel / spolecny_delitel;
+
+    // ji-li citatel i jmenovatel zporny, celkovy zlomek je kladny a muzeme se tedy zbavit znamenek
+    if (this->citatel < 0 && this->jmenovatel < 0) { 
+        this->citatel    *= -1; 
+        this->jmenovatel *= -1; 
+    } 
+    
+    if (this->jmenovatel < 0 && this->citatel > 0) {
+        this->jmenovatel *= -1;
+        this->citatel    *= -1;
+    }
 }
 /**
  * @brief  friend metoda pro vypis objektu zlomku (vypis bude vypadata takto => (-)citatel/jmenovatel)
@@ -78,30 +78,27 @@ void Zlomek::zkrat(void) {
  * @return std::ostream& 
  */
 std::ostream &operator << (std::ostream &output, Zlomek content) {
-	// v pripde, ze jmenovatel je jedna vypiseme pouze citatel
-	if (content.jmenovatel == 1) {
-		output << content.citatel << '\n';
-		
-		return output;
-	}
+    if (content.jmenovatel == 1) {
+        output << content.citatel;
+    }
 
-	// v pripade, ze ve vysledek vysel nula
-	if (content.citatel == 0 && content.jmenovatel == 0) {
-		output << "0\n";
-		
-		return output;
-	}
-	
-	// v pripade, ze vysledek vysel jedna 
-	if (content.citatel == content.jmenovatel) {
-		output << "1\n";
-		
-		return output;
-	}
-	
-	output << content.citatel << "/" << content.jmenovatel << '\n';
-	
-	return output;
+    // v pripade, ze ve vysledek vysel nula
+    if (content.citatel == 0 && content.jmenovatel == 0) {
+        output << "0";
+
+        return output;
+    }
+
+    // v pripade, ze vysledek vysel jedna 
+    if (abs(content.citatel) == abs(content.jmenovatel)) {
+        content.citatel > 0 ? output << "1" : output << "-1";
+
+        return output;
+    }
+
+    output << content.citatel << "/" << content.jmenovatel;
+
+    return output;
 }
 /**
  * @brief friend metoda pro ziskani objektu zlomku
@@ -111,13 +108,13 @@ std::ostream &operator << (std::ostream &output, Zlomek content) {
  * @return std::istream& 
  */
 std::istream &operator >> (std::istream& input, Zlomek& content) {
-	input >> content.citatel;
-	do {
-		input >> content.jmenovatel;
-		
-		if (content.jmenovatel == 0) { std::cout << "nelze delit nulou."; }
-		
-	} while (content.jmenovatel == 0);
+    input >> content.citatel;
+    do {
+        input >> content.jmenovatel;
+
+        if (content.jmenovatel == 0) { std::cout << "nelze delit nulou."; }
+
+    } while (content.jmenovatel == 0);
 }
 /**
  * @brief friend metoda pro scitani dvou objektu tridy zlomek 
@@ -127,14 +124,14 @@ std::istream &operator >> (std::istream& input, Zlomek& content) {
  * @return Zlomek 
  */
 Zlomek operator + (const Zlomek& cislo1, const Zlomek& cislo2) {
-	Zlomek vysledek;
-	
-	vysledek.citatel = (cislo1.citatel * cislo2.jmenovatel) + (cislo2.citatel * cislo1.jmenovatel);
-	vysledek.jmenovatel = cislo1.jmenovatel * cislo2.jmenovatel;
-	
-	vysledek.zkrat();   // upraveni zlomku do zakladniho tvaru funkci zkrat()
-	
-	return vysledek;
+    Zlomek vysledek;
+
+    vysledek.citatel = (cislo1.citatel * cislo2.jmenovatel) + (cislo2.citatel * cislo1.jmenovatel);
+    vysledek.jmenovatel = cislo1.jmenovatel * cislo2.jmenovatel;
+
+    vysledek.zkrat();   // upraveni zlomku do zakladniho tvaru funkci zkrat()
+
+    return vysledek;
 }
 /**
  * @brief  friend metoda pro odcitani dvou objektu tridy zlomek 
@@ -144,21 +141,21 @@ Zlomek operator + (const Zlomek& cislo1, const Zlomek& cislo2) {
  * @return Zlomek 
  */
 Zlomek operator - (const Zlomek& cislo1, const Zlomek& cislo2) {
-	Zlomek vysledek;
-	
-	if (cislo1.citatel == cislo2.citatel && cislo1.jmenovatel == cislo2.jmenovatel) { 
-		vysledek.citatel = 0;
-		vysledek.jmenovatel = 0;
-		
-		return vysledek;
-	}
-	
-	vysledek.citatel = (cislo1.citatel * cislo2.jmenovatel) - (cislo2.citatel * cislo1.jmenovatel);
-	vysledek.jmenovatel = cislo1.jmenovatel * cislo2.jmenovatel;
-	
-	vysledek.zkrat();
-	
-	return vysledek;
+    Zlomek vysledek;
+
+    if (cislo1.citatel == cislo2.citatel && cislo1.jmenovatel == cislo2.jmenovatel) { 
+        vysledek.citatel = 0;
+        vysledek.jmenovatel = 0;
+
+        return vysledek;
+    }
+
+    vysledek.citatel = (cislo1.citatel * cislo2.jmenovatel) - (cislo2.citatel * cislo1.jmenovatel);
+    vysledek.jmenovatel = cislo1.jmenovatel * cislo2.jmenovatel;
+
+    vysledek.zkrat();
+
+    return vysledek;
 }
 /**
  * @brief  friend metoda pro nasobeni dvou objektu tridy zlomek 
@@ -168,15 +165,15 @@ Zlomek operator - (const Zlomek& cislo1, const Zlomek& cislo2) {
  * @return Zlomek 
  */
 Zlomek operator * (const Zlomek& cislo1, const Zlomek& cislo2) {
-	Zlomek vysledek;
-	
-	vysledek.citatel = cislo1.citatel * cislo2.citatel;
-	vysledek.jmenovatel = cislo1.jmenovatel * cislo2.jmenovatel;
-	
-	
-	vysledek.zkrat();
-	
-	return vysledek;
+    Zlomek vysledek;
+
+    vysledek.citatel = cislo1.citatel * cislo2.citatel;
+    vysledek.jmenovatel = cislo1.jmenovatel * cislo2.jmenovatel;
+
+
+    vysledek.zkrat();
+
+    return vysledek;
 }
 /**
  * @brief  friend metoda pro deleni dvou objektu tridy zlomek 
@@ -186,12 +183,64 @@ Zlomek operator * (const Zlomek& cislo1, const Zlomek& cislo2) {
  * @return Zlomek 
  */
 Zlomek operator / (const Zlomek& cislo1, const Zlomek& cislo2) {
-	Zlomek vysledek;
-	
-	vysledek.citatel = cislo1.citatel * cislo2.jmenovatel;
-	vysledek.jmenovatel = cislo1.jmenovatel * cislo2.citatel;
-	
-	vysledek.zkrat();
-	
-	return vysledek;
+    Zlomek vysledek;
+
+    vysledek.citatel = cislo1.citatel * cislo2.jmenovatel;
+    vysledek.jmenovatel = cislo1.jmenovatel * cislo2.citatel;
+
+    vysledek.zkrat();
+
+    return vysledek;
+}
+/**
+ * @brief friend funkce pro porovnani dvou zlomku
+ * 
+ * zlomek1 > zlomke2 => true
+ * 
+ * @param zlomek1 
+ * @param zlomek2 
+ * @return true 
+ * @return false 
+ */
+bool operator > (const Zlomek& zlomek1, const Zlomek& zlomek2) {
+    return ((zlomek2.jmenovatel * zlomek1.citatel) > (zlomek1.jmenovatel * zlomek2.citatel)) ? true : false;
+}
+/**
+ * @brief friend funkce pro porovnani dvou zlomku
+ * 
+ * zlomek1 < zlomke2 => true
+ * 
+ * @param zlomek1 
+ * @param zlomek2 
+ * @return true 
+ * @return false 
+ */
+bool operator < (const Zlomek& zlomek1, const Zlomek& zlomek2) {
+    return ((zlomek2.jmenovatel * zlomek1.citatel) < (zlomek1.jmenovatel * zlomek2.citatel)) ? true : false;
+}
+/**
+ * @brief friend funkce pro porovnani dvou zlomku
+ * 
+ * zlomek1 == zlomke2 => true
+ * 
+ * @param zlomek1 
+ * @param zlomek2 
+ * @return true 
+ * @return false 
+ */
+bool operator == (const Zlomek& zlomek1, const Zlomek& zlomek2) {
+    return ((zlomek2.jmenovatel * zlomek1.citatel) == (zlomek1.jmenovatel * zlomek2.citatel)) ? true : false;
+}
+/**
+ * @brief friend funkce pro porovnani dvou zlomku
+ * 
+ * zlomek1 != zlomke2 => true
+ * 
+ * @param zlomek1 
+ * @param zlomek2 
+ * @return true 
+ * @return false 
+ */
+bool operator != (const Zlomek& zlomek1, const Zlomek& zlomek2) {
+    return ((zlomek2.jmenovatel * zlomek1.citatel) != (zlomek1.jmenovatel * zlomek2.citatel)) ? true : false;
 }
